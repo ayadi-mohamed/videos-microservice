7baf21ee0cadb0afe5ef40050f529787d9870357
OumaymaCharrad1999 <charradoumayma1@gmail.com>
2023-12-22 22:09:00 +0000

    Increment Version to 1.0.1

--- app.go
+++ app.go
@@ -19,7 +19,7 @@ import (
 )
 
 const metricsAddr = ":8000"
-const version = "1.0.0"
+const version = "1.0.1"
 var environment = os.Getenv("ENVIRONMENT")
 var redisHost = os.Getenv("REDIS_HOST")
 var redisPort = os.Getenv("REDIS_PORT")
